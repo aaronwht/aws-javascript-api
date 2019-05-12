0a9da71d39308260b3fff23af6b2c6120f131b03
aaronwht <awhite@tarvold.com>
2019-05-12 15:03:50 -0700

    Updated API end points

--- app.js
+++ app.js
@@ -110,69 +110,39 @@ app.delete('/:id', (req, res) => {
     })
 })
 
-// app.get('/create-table', (req, res) => {
-//     const params ={
-//         "TableName": "Locations",
-//         "KeySchema": [
-//           {
-//             "AttributeName": "location_id",
-//             "KeyType": "HASH"
-//           }
-//         ],
-//         "AttributeDefinitions": [
-//           {
-//             "AttributeName": "location_id",
-//             "AttributeType": "S"
-//           }
-//         ],
-//         "ProvisionedThroughput": {
-//           "ReadCapacityUnits": 5,
-//           "WriteCapacityUnits": 5
-//         }
-//       }
+app.get('/create-table', (req, res) => {
+    const params = {
+        "TableName": "Locations",
+        "KeySchema": [
+            {
+                "AttributeName": "location_id",
+                "KeyType": "HASH"
+            }
+        ],
+        "AttributeDefinitions": [
+            {
+                "AttributeName": "location_id",
+                "AttributeType": "S"
+            }
+        ],
+        "ProvisionedThroughput": {
+            "ReadCapacityUnits": 5,
+            "WriteCapacityUnits": 5
+        }
+    }
 
-//     dynamodb.createTable(params, (err, data) => {
-//         if (err) return res.json({status: 'error', err })
-//         return res.json({status: 'success', data })
-//     })
-// })
+    dynamodb.createTable(params, (err, data) => {
+        if (err) return res.json({ status: 'error', err })
+        return res.json({ status: 'success', data })
+    })
+})
 
-// app.get('/create-location', (req, res) => {
-//     const params = {
-//         Item: {
-//             location_id: uuidv4(),
-//             title: "Hawaii",
-//             country: "US"
-//         },
-//         TableName: "Locations",
-//     }
+app.get('/delete-table', (req, res) => {
+    dynamodb.deleteTable({ TableName: "Locations" }, (err, data) => {
+        if (err) return res.json({ status: 'error', err })
 
-//     clientdb.put(params, (err, data) => {
-//         if (err) return res.json({ status: 'error', err })
-
-//         res.json({ status: 'success', data })
-//     })
-// })
-
-// app.get('/delete-table', (req, res) => {
-//     dynamodb.deleteTable({ TableName: "Locations"}, (err, data) => {
-//         if (err) return res.json({ status: 'error', err })
-
-//         return res.send({ status: 'success', message: 'Table deleted'})
-//     })
-// })
-
-// app.get('/delete-location/:id', (req, res) => {
-//     dynamodb.deleteItem({
-//         Key: {
-//             location_id: { S: req.params.id }
-//         },
-//         TableName: "Locations",
-//     },  (err, data)  => {
-//       if (err) return res.json({ err })
-
-//       return res.json({ data })
-//     })
-// })
+        return res.send({ status: 'success', message: 'Table deleted' })
+    })
+})
 
 http.createServer(app).listen(process.env.PORT || 8080)
